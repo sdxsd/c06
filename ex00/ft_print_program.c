9d8d7568c44b15c64c18da0fc8dd2a74184548a1
sdxsd <willmaguire@icloud.com>
2021-07-28 13:17:39 +0200

    Fixed norminette errors

--- ex00/ft_print_program.c
+++ ex00/ft_print_program.c
@@ -1,16 +1,17 @@
 #include <unistd.h>
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    char *ptr;
-    int iterator;
+	char	*ptr;
+	int		iterator;
 
-    ptr = argv[0];
-    iterator = 0;
-    if (argc){};
-    while (ptr[iterator] != '\0')
-    {
-       ++iterator;
-    }
-    write(1, ptr, iterator);
+	ptr = argv[0];
+	iterator = 0;
+	if (argc)
+		;
+	while (ptr[iterator] != '\0')
+	{
+		++iterator;
+	}
+	write(1, ptr, iterator);
 }
--- ex03/ft_rev_params.c
+++ ex03/ft_rev_params.c
@@ -1,26 +1,26 @@
 #include <unistd.h>
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
-    int iterator;
+	int	iterator;
 
-    iterator = 0;
-    while (str[iterator] != '\0')
-    {
-        iterator++;
-    }
-    write(1, str, iterator);
+	iterator = 0;
+	while (str[iterator] != '\0')
+	{
+		iterator++;
+	}
+	write(1, str, iterator);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    int iterator;
+	int	iterator;
 
-    iterator = argc - 1;
-    while (iterator != 0)
-    {
-        ft_putstr(argv[iterator]);
-        write(1, "\n", 1);
-        --iterator;
-    }
+	iterator = argc - 1;
+	while (iterator != 0)
+	{
+		ft_putstr(argv[iterator]);
+		write(1, "\n", 1);
+		--iterator;
+	}
 }
